649d135526baa59ae0f6353c742707e34bd74ca2
Sri Chaahna G <131888691+srichaahna@users.noreply.github.com>
2023-06-06 15:14:45 +0530

    Create chatbot.py
    This is an Info aidTech virtual internship for the Artificial Intelligence. Compeltion of task-1(chatbot).

---
+++ chatbot.py
@@ -0,0 +1,25 @@
+import nltk
+from nltk.chat.util import Chat, reflections
+
+# Define patterns and responses for the chatbot
+patterns = [
+    (r"hi|hello|hey", ["Hello!", "Hi there! How can I help you?"]),
+    (r"bye|goodbye", ["Goodbye!", "See you later!"]),
+    (r"my name is (.*)", ["Hello, %1! How can I assist you?"]),
+    (r"(.*) (weather|forecast)", ["The weather is sunny today."]),
+    (r"(.*) help (.*)", ["Sure, I can help. What do you need assistance with?"]),
+    (r"(.*) (remedies)", ["drink lot of water and use umbrella while going out."])
+    # Add more patterns and responses as needed
+]
+
+# Create a chatbot using the defined patterns
+chatbot = Chat(patterns, reflections)
+
+# Start the conversation with the user
+print("Chatbot: Hello! How can I assist you today?")
+while True:
+    user_input = input("User: ")
+    if user_input.lower() == "quit":
+        break
+    response = chatbot.respond(user_input)
+    print("Chatbot:", response)
